fb90c5fc0cde09339b1de4d5b5ca8ca3d4f60cd8
Ako-in <sinba.805i@gmail.com>
2024-02-20 18:55:51 +0700

    bmi.jsを修正

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -2,11 +2,13 @@
 // 身長：1.7m
 // 計算式：[体重(kg)]÷[身長(m)×身長(m)]
 
-let weight;
-weight = 68;
-let tall;
-tall = 1.7;
+// let weight;
+// weight = 68;
+// let tall;
+// tall = 1.7;
 
+let weight = 68;
+let tall = 1.7;
 console.log(weight / (tall * tall));
 
 
